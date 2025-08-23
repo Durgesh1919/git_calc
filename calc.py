f0a7a4a4d2a23b28ac3f30f3ed8b93b956907a17
Durgesh Nikam <durgeshnikam2003@gmail.com>
2025-08-23 17:02:14 +0530

    add

--- calc.py
+++ calc.py
@@ -1,12 +1,2 @@
-# calculator
+print("calculator")
 
-print ("claculator")
-print ("_"*30)
-print ("the choices \n1. Addition \n2.subtraction")
-choice = int(input("enter your choice : "))
-a = int(input("enter the first number : "))
-b = int(input("enter the second number : "))
-if choice ==1:
-    print("addition is : ",a+b)
-elif choice == 2:
-    print("subtraction is :  ",a-b)
