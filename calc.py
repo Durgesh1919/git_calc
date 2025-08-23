b8ba2379e2dbf40198739a705e9fa966fa6fc037
Durgesh Nikam <durgeshnikam2003@gmail.com>
2025-08-23 19:12:07 +0530

    add div mult

--- calc.py
+++ calc.py
@@ -2,7 +2,7 @@
 
 print ("claculator")
 print ("_"*30)
-print ("the choices \n1. Addition \n2.subtraction")
+print ("the choices \n1. Addition \n2.subtraction \n3.multiplication \n4.division")
 choice = int(input("enter your choice : "))
 a = int(input("enter the first number : "))
 b = int(input("enter the second number : "))
@@ -10,3 +10,8 @@ if choice ==1:
     print("addition is : ",a+b)
 elif choice == 2:
     print("subtraction is :  ",a-b)
+elif choice == 3:
+    print("multiplecation is : ",a*b)
+elif choice == 4:
+    print(f"division is : {a/b}")
+
